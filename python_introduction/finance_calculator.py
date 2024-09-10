4df972ff60fa1fdaa8521be6fe51475119566fd3
solianadaniel1 <solianadaniel22@gmail.com>
2024-09-10 01:28:58 -0700

    calculate users monthly savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,7 +4,7 @@ income = int(input("Enter your monthly income:"))
 expense = int(input("Enter your monthly expenses:"))
 
 monthly_savings = abs(expense - income)
-savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+savings = abs(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
-print(f"Your monthly savings are ${monthly_savings}")
-print(f"Projected savings after one year, with interest, is: ${savings}")
+print(f"Your monthly savings are ${int(monthly_savings)}")
+print(f"Projected savings after one year, with interest, is: ${int(savings)}")

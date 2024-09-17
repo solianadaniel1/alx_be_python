e08f784851d82d2179764370d0d58c9bfad6ff41
solianadaniel1 <solianadaniel22@gmail.com>
2024-09-17 01:01:36 -0700

    Daily reminder based on priority

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 
-# Prompt the user for task details
-task = input("Enter task description: ")
-priority = input("Priority (high/medium/low): ").lower()
-time_bound = input("Is it time-bound? (yes/no): ").lower()
+task = input("Enter task description")
+priority = input("Priority (high/medium/low) ")
+time_bound = input("Is it time-bound?(yes/no) ")
 
-# Provide customized reminder based on priority and time sensitivity
 match priority:
     case "high":
-        if time_bound == "yes":
+        if time_bound:
             print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
-            print(f"Note: '{task}' is a high priority task. Consider completing it when you have free time.")
+            print(f"Note:'{task}' is a low priority task. Consider completing it when you have free time.")   
     case "medium":
-        if time_bound == "yes":
-            print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
+        if time_bound:
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
-            print(f"Note: '{task}' is a medium priority task. Consider completing it when you have free time.")
+            print(f"Note:'{task}' is a low priority task. Consider completing it when you have free time.")
+            
     case "low":
-        if time_bound == "yes":
-            print(f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
+        if time_bound:
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
             print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
-    case _:
+    case _: 
         print("Invalid priority level")
-

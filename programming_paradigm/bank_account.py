cb9a603bc7d20aee64fd35a43fc313bbe83f79d6
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-01 00:05:58 -0700

    Bank Transactions

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,15 +7,15 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
-            return f"Deposited: ${amount:.2f}"
+            return f"Deposited: ${account_balance:.2f}"
         return "Invalid deposit amount"
 
     def withdraw(self, amount):
         if 0 < amount <= self.account_balance:
             self.account_balance -= amount
-            return f"Withdrew: ${amount:.2f}"
-        return "Insufficient funds."  # Ensure this is returned if funds are insufficient
+            return f"Withdrew: ${account_balance:.2f}"
+        return "Insufficient funds or invalid amount"
 
     def display_balance(self):
-        return f"Your Current Balance is: ${self.account_balance:.2f}"
+        return f"Current Balance: ${self.amount:.2f}"
 

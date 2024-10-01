063bba9911c02f2a88f87042b777f6ec4ac86aad
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-01 01:03:30 -0700

    Division Calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+    
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        dem = float(denominator)
+        x = num/dem
+        return f"The result of the division is {x}"
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
+    except ValueError:
+        print("Invalid Input not a numeric value")

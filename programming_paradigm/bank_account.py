f4354f4511da097fd277479e5a4bee5f3da4d736
solianadaniel1 <solianadaniel22@gmail.com>
2024-09-30 23:51:01 -0700

    Bank Transactions

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+    def init(self, account_balance= 0):
+        self.account_balance = account_balance
 
-    def deposit(self, amount):
+    def deposit(self,amount):
         if amount > 0:
-            self.account_balance += amount
+           self.account_balance += amount
 
     def withdraw(self, amount):
-        # Make sure there's a colon after the if statement
         if 0 < amount <= self.account_balance:
             self.account_balance -= amount
             return True
         return False
 
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}")
-
+        print(f"Current Balance:{amount}")

00d499de235e7f935f54f6600687c45815d5897b
solianadaniel1 <solianadaniel22@gmail.com>
2024-09-30 23:59:26 -0700

    Bank Transactions

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
 class BankAccount:
-    def init(self, account_balance):
+    def __init__(self, account_balance=0):
         self.account_balance = account_balance
 
-    def deposit(self,amount):
+    def deposit(self, amount):
         if amount > 0:
-           self.account_balance += amount
+            self.account_balance += amount
+            return f"Deposited: ${amount}"
+        return "Invalid deposit amount"
 
     def withdraw(self, amount):
         if 0 < amount <= self.account_balance:
             self.account_balance -= amount
-            return True
-        return False
+            return f"Withdrew: ${amount}"
+        return "Insufficient funds or invalid amount"
 
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        return f"Your Current Balance is: ${self.account_balance}"
+

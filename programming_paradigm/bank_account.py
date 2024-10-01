86125a89f8b1ec30a8d4a01171d44a1336dbca78
solianadaniel1 <solianadaniel22@gmail.com>
2024-09-30 23:17:15 -0700

    Bank Transactions

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class BankAccount:
+    def __init__(self, account_balance ):
+        self.account_balance = account_balance 
+
+    def deposit(self,amount):
+        if amount > 0:
+           self.account_balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Your Current Balance is {self.account_balance}")

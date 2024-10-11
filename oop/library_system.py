59d536811069dec8a5d9a560dca2056b89f24d62
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-11 00:37:58 -0700

    Inheritance in python

--- oop/library_system.py
+++ oop/library_system.py
@@ -9,14 +9,14 @@ class Book:
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(file_size)
+        super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
 
 class PrintBook(Book):
     def __init__(self,title, author, page_count):
-        super().__init__(page_count)
+        super().__init__(title, author)
         self.page_count = page_count
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"

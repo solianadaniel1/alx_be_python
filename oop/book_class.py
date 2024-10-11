7e3f541725362fdf243672d2212ca3bd5e54e357
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-10 23:24:00 -0700

    Magic Methods in python

--- oop/book_class.py
+++ oop/book_class.py
@@ -8,6 +8,6 @@ class Book:
     def __del__(self):
         print(f"Deleting: {self.title}")
     def __str__(self):
-        print(f" {self.title} by {self.author}, published in {self.year}")
+        return f" {self.title} by {self.author}, published in {self.year}"
     def __rep__(self):
-        print(f"Book('{self.title}', '{self.author}', '{self.year}')".")
+        return f"Book('{self.title}', '{self.author}', '{self.year}')"."

d8c2f59c0a57191def95be5451e6a65bc36d7d18
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-11 00:00:33 -0700

    Inheritance in python

--- oop/library_system.py
+++ oop/library_system.py
@@ -23,10 +23,10 @@ class PrintBook(Book):
 
 class Library:
     def __init__(self, book):
-        self.book = []
+        self.books = []
     def add_book(self, book):
-        self.book.append(book)
-    def list_book(self):
-        for book in self.book:
+        self.books.append(book)
+    def list_books(self):
+        for book in self.books:
             print(book)
         

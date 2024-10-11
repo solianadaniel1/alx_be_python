73154b1fa4d9e9ba2853d5f1d7539e59f66d382d
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-10 23:36:31 -0700

    Magic method in python

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,9 +5,15 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
+
     def __del__(self):
         print(f"Deleting {self.title}")
+
     def __str__(self):
-        return f"{self.title} by {self.author},published in {self.year}"
+        # Make sure there are no extra spaces
+        return f"{self.title} by {self.author}, published in {self.year}"
+
     def __repr__(self):
-        return f"Book('{self.title}','{self.author}',{self.year})"
+        # Make sure there are no extra spaces
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+

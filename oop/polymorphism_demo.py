185ce4e01be65b0c5e61c50e2bf0f8f5fbb9507c
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-11 00:53:07 -0700

    Polymorphism in python

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -16,6 +16,6 @@ class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
     def area(self):
-        return math.pi * (self.radius **2)
+        return math.pi * (self.radius ** 2)
         
 

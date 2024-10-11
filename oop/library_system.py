b083a7f4b795928179db8f321ae3a32eed991fcd
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-10 23:58:53 -0700

    Inheritance in python

---
+++ oop/library_system.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(file_size)
+        self.file_size = file_size
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
+
+class PrintBook(Book):
+    def __init__(self,title, author, page_count):
+        super().__init__(page_count)
+        self.page_count = page_count
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+class Library:
+    def __init__(self, book):
+        self.book = []
+    def add_book(self, book):
+        self.book.append(book)
+    def list_book(self):
+        for book in self.book:
+            print(book)
+        

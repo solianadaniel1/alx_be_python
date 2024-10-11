f988af153ea0fca8d19e341173924cdb3ef2b5e0
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-10 23:26:29 -0700

    Magic Methods in python

--- oop/book_class.py
+++ oop/book_class.py
@@ -10,4 +10,4 @@ class Book:
     def __str__(self):
         return f" {self.title} by {self.author}, published in {self.year}"
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', '{self.year}')"."
+        return f"Book('{self.title}', '{self.author}', '{self.year}')"
